cdff02e66041a38cdd80286eb17c2bacb80e5c5d
zwyu123 <zwyu2017@163.com>
2019-04-29 00:29:25 +0800

    Druid、Freemarker

---
+++ src/main/java/com/imooc/house/config/DruidConfig.java
@@ -0,0 +1,37 @@
+package com.imooc.house.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.google.common.collect.Lists;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.druid")
+    @Bean(initMethod = "init",destroyMethod = "close")
+    public DruidDataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setProxyFilters(Lists.newArrayList(statFilter()));
+        return dataSource;
+    }
+
+    @Bean
+    public Filter statFilter(){
+        StatFilter filter = new StatFilter();
+        filter.setSlowSqlMillis(1);
+        filter.setLogSlowSql(true);
+        filter.setMergeSql(true);
+        return filter;
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+    }
+}
---
+++ src/main/java/com/imooc/house/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.imooc.house.controller;
+
+import com.imooc.house.common.model.User;
+import com.imooc.house.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("hello")
+    public String hello(ModelMap modelMap){
+        List<User> users = userService.getUsers();
+        User one = users.get(0);
+        modelMap.put("user",one);
+        return "hello";
+    }
+
+    @RequestMapping("index")
+    public String index(){
+        return "homepage/index";
+    }
+}
